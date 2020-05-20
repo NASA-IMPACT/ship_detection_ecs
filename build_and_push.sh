cd62046a0313326774178c691732ce72eef3aec4
xhagrg <grg.iksha@hotmail.com>
2020-05-20 15:50:18 -0500

    Reuse variable.

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-
-docker build . -f Dockerfile -t nasa-impact/ship_detection:$VERSION \
+echo $AWS_SECRET_ACCESS_KEY
+docker build . -f Dockerfile -t ship_detection:$VERSION \
  --build-arg AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
  --build-arg AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 
-if [ $PUSH = '1' ]
+if [[ $PUSH = '1' ]]
 then
-  aws ecr get-login --region us-east-1 --no-include-email | source /dev/stdin
+  export ECR_URL="853558080719.dkr.ecr.us-east-1.amazonaws.com"
+  aws ecr get-login-password --region us-east-1 --profile covid-response | docker login --username AWS --password-stdin $ECR_URL
 
-  docker tag nasa-impact/ship_detection:$VERSION 853558080719.dkr.ecr.us-east-1.amazonaws.com/ship_detection:latest
-  docker push 853558080719.dkr.ecr.us-east-1.amazonaws.com/ship_detection:latest
+  docker tag ship_detection:$VERSION $ECR_URL/ship_detection:latest
+  docker push $ECR_URL/ship_detection:latest
 fi
