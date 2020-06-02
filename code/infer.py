160afb9a88930c7a220254af2e896c1002be98dd
xhagrg <grg.iksha@hotmail.com>
2020-06-02 16:43:33 -0500

    Update model path and remove unwanted image save.

--- code/infer.py
+++ code/infer.py
@@ -23,11 +23,6 @@ from PIL import (
 from planet_downloader import PlanetDownloader
 from skimage.measure import regionprops
 
-
-WEIGHT_FILE = '../weights/iou_model.hdf5'
-
-
-WMTS_URL = f"https://tiles1.planet.com/data/v1/PSScene3Band/{{}}/{ZOOM_LEVEL}/{{}}/{{}}.png?api_key={{}}"
 GEOJSON_TEMPLATE = {
     "type": "Feature",
     "properties": {},
@@ -37,10 +32,13 @@ GEOJSON_TEMPLATE = {
     }
 }
 
+# had to do this because of how we are running the script
+WEIGHT_FILE = '/ship_detection/weights/iou_model.hdf5'
+WMTS_URL = f"https://tiles1.planet.com/data/v1/PSScene3Band/{{}}/{ZOOM_LEVEL}/{{}}/{{}}.png?api_key={{}}"
+
 class Infer:
 
-    def __init__(self, date, weight_path=WEIGHT_FILE, credential=None):
-        self.start_date_time, self.end_date_time = self.prepare_date(date)
+    def __init__(self, weight_path=WEIGHT_FILE, credential=None):
         self.weight_path = weight_path
         self.model = self.prepare_model()
         self.credential = credential
@@ -55,23 +53,29 @@ class Infer:
         return load_from_path(self.weight_path)
 
 
-    def infer(self):
+    def infer(self, date):
+        self.start_date_time, self.end_date_time = self.prepare_date(date)
         detections = list()
         for location, extent  in EXTENTS.items():
             items = self.planet_downloader.search_ids(
                 extent, self.start_date_time, self.end_date_time
             )
             for item in items:
+                print(f"id: {item['id']}")
                 images = self.prepare_dataset(item['tiles'], item['id'])
                 predictions = self.model.predict((images / 255.))
                 columns, rows = [elem[1] - elem[0] for elem in item['tiles']]
-                predictions = predictions.reshape((rows, columns, IMG_SIZE, IMG_SIZE))
-                images = images.reshape((rows, columns, IMG_SIZE, IMG_SIZE, 3))
+                predictions = predictions.reshape(
+                    (rows, columns, IMG_SIZE, IMG_SIZE)
+                )
+                images = images.reshape(
+                    (rows, columns, IMG_SIZE, IMG_SIZE, 3)
+                )
                 polygons = self.xy_to_latlon(
                     predictions, images, rows, columns, item['coordinates']
                 )
                 detections.extend(polygons)
-        return { "type": "FeatureCollection", "features": detections }
+        return { 'type': 'FeatureCollection', 'features': detections }
 
     def prepare_dataset(self, tile_range, tile_id):
         x_indices, y_indices = tile_range
@@ -81,7 +85,6 @@ class Infer:
             response = requests.get(
               WMTS_URL.format(tile_id, x_index, y_index, self.credential)
             )
-            print(WMTS_URL.format(tile_id, x_index, y_index, self.credential))
             response.raise_for_status()
             img = np.asarray(
               Image.open(BytesIO(response.content)).resize(
@@ -106,20 +109,29 @@ class Infer:
         rows, colms, _, _ = np.where(grid_list >= THRESHOLD)
         for row, col in set(zip(rows, colms)):
             segments = (grid_list[row][col] > THRESHOLD).astype('uint8')
-            img = Image.fromarray(images[row][col])
-            draw = ImageDraw.Draw(img)
+            # keeping this for when we need to save images.
+            # img = Image.fromarray(images[row][col])
+            # draw = ImageDraw.Draw(img)
             for idx, ship in enumerate(regionprops(segments)):
                 bbox = ship.bbox
                 xs = bbox[::2]
                 ys = bbox[1::2]
-                draw.rectangle([(xs[0], ys[0]), (xs[1], ys[1])], fill ="#ffff33", outline ="red")
+                draw.rectangle(
+                    [
+                        (xs[0], ys[0]),
+                        (xs[1], ys[1])
+                    ],
+                    fill ='#ffff33',
+                    outline ='red'
+                )
                 lons, lats = rasterio.transform.xy(
                     transform, (col * TILE_SIZE) + xs, (row * TILE_SIZE) + ys
                 )
                 reformated_bbox = self.planet_downloader.prepare_coordinates(
                     [lons[0], lats[0], lons[1], lats[1]]
                 )
-                polygon_coordinates.append(self.prepare_geojson(reformated_bbox))
-            img.save(f"{row}_{col}.png")
-            print(f"{row}_{col}.png")
+                polygon_coordinates.append(
+                    self.prepare_geojson(reformated_bbox)
+                )
+            # img.save(f"{row}_{col}.png")
         return polygon_coordinates
