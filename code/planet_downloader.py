a8d2eb046d6ffc1753aff4ec9415fcb83a44b350
xhagrg <grg.iksha@hotmail.com>
2020-08-07 11:06:47 -0500

    Fix lint errors.

--- code/planet_downloader.py
+++ code/planet_downloader.py
@@ -1,8 +1,10 @@
 import json
-import requests
-import pyproj
+
 import mercantile
+
 import numpy as np
+
+import requests
 
 from config import ZOOM_LEVEL
 
@@ -10,61 +12,66 @@ from config import ZOOM_LEVEL
 BODY = {
     "filter": {
         "type": "AndFilter",
-        "config": [{
-            "type": "GeometryFilter",
-            "field_name": "geometry",
-            "config": {
-                  "type": "Polygon",
-                  "coordinates": []
-               }
-        },
-        {
-            "type": "DateRangeFilter",
-            "field_name":"acquired",
-                "config":{
-                    "gt": "",
-                    "lte": ""
-             }
-        },
-        {
-            "type": "RangeFilter",
-            "field_name": "cloud_cover",
-            "config": {
-                "lte": 0.5
+        "config": [
+            {
+                "type": "GeometryFilter",
+                "field_name": "geometry",
+                "config": {
+                    "type": "Polygon",
+                    "coordinates": []
+                }
+            },
+            {
+                "type": "DateRangeFilter",
+                "field_name": "acquired",
+                "config": {
+                        "gt": "",
+                        "lte": ""
+                }
+            },
+            {
+                "type": "RangeFilter",
+                "field_name": "cloud_cover",
+                "config": {
+                    "lte": 0.5
+                }
             }
-        }]
+        ]
     },
     "item_types": ["PSScene3Band"]
 }
 
 SEARCH_URL = 'https://api.planet.com/data/v1/quick-search'
 
+
 class PlanetDownloader:
 
     def __init__(self, api_key):
         self.api_key = api_key
 
-
     def search_ids(self, extent, start_date_time, end_date_time):
         body = BODY
-        body['filter']['config'][0]['config']['coordinates'] = self.prepare_coordinates(
-          extent
+        body['filter']['config'][0]['config']['coordinates'] = \
+            self.prepare_coordinates(
+                extent
         )
-        body['filter']['config'][1]['config']['gt'] = start_date_time
-        body['filter']['config'][1]['config']['lte'] = end_date_time
-        print(start_date_time, end_date_time)
-        response = requests.post(SEARCH_URL, auth=(self.api_key, ''), json=body)
+        body['filter']['config'][1]['config'] = {
+            'gt': start_date_time,
+            'lte': end_date_time
+        }
+        response = requests.post(
+            SEARCH_URL, auth=(self.api_key, ''), json=body
+        )
         # if not 200 raise error
         response.raise_for_status()
         parsed_content = json.loads(response.text)
 
         return self.extract_data(parsed_content['features'])
 
-
     def extract_data(self, parsed_data):
         extracted_data = []
         for feature in parsed_data:
-            current_data = { 'images': [] }
+            current_data = {'images': []}
             current_data['id'] = feature['id']
             reverted_coordinates = self.revert_coordinates(
                 feature['geometry']['coordinates']
@@ -74,19 +81,24 @@ class PlanetDownloader:
             extracted_data.append(current_data)
         return extracted_data
 
-
     def revert_coordinates(self, coordinates):
         coordinates = np.asarray(coordinates)
-        lats = coordinates[:,:,1]
-        lons = coordinates[:,:,0]
+        lats = coordinates[:, :, 1]
+        lons = coordinates[:, :, 0]
         return [lons.min(), lats.min(), lons.max(), lats.max()]
 
-
     def tile_indices(self, coordinates):
-        start_x, start_y, _ = mercantile.tile(coordinates[0], coordinates[3], ZOOM_LEVEL)
-        end_x, end_y, _ = mercantile.tile(coordinates[2], coordinates[1], ZOOM_LEVEL)
+        start_x, start_y, _ = mercantile.tile(
+            coordinates[0],
+            coordinates[3],
+            ZOOM_LEVEL
+        )
+        end_x, end_y, _ = mercantile.tile(
+            coordinates[2],
+            coordinates[1],
+            ZOOM_LEVEL
+        )
         return [[start_x, end_x], [start_y, end_y]]
-
 
     def prepare_coordinates(self, extent):
         return [
